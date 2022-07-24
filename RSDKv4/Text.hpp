0b87b273adf3b2bacaf3a5e7f5e56b8d494f44cf
ElspethThePict <77791867+ElspethThePict@users.noreply.github.com>
2022-07-24 20:00:24 +0100

    five text menus instead of two. Fuck it

--- RSDKv4/Text.hpp
+++ RSDKv4/Text.hpp
@@ -3,7 +3,7 @@
 
 #define TEXTDATA_COUNT  (0x2800)
 #define TEXTENTRY_COUNT (0x200)
-#define TEXTMENU_COUNT  (0x2)
+#define TEXTMENU_COUNT  (0x5)
 
 #define FONTLIST_CHAR_COUNT (0x1000)
 #define FONTLIST_COUNT      (0x4)
